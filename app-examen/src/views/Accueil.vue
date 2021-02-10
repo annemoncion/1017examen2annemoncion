b10257d64c1fbe05a27af70078bf9602fbcfbcf6
Anne Moncion <anne.moncion@sigmund.ca>
2021-02-09 20:21:15 -0500

    hook created

--- app-examen/src/views/Accueil.vue
+++ app-examen/src/views/Accueil.vue
@@ -1,23 +1,51 @@
 <template>
-  <div class="accueil">
-    <h1>Ma page d'accueil</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at elementum turpis, id pharetra arcu. Aenean egestas faucibus felis, suscipit tempus felis condimentum consequat. Sed vel dui turpis. Nam vitae dui vitae mi lacinia congue. Donec et magna nec odio iaculis euismod. Sed sit amet pharetra ante. Suspendisse tincidunt pulvinar ligula, pellentesque porta odio sodales sed. Donec iaculis nibh nec lectus pellentesque, vel euismod metus lobortis. Sed tincidunt tortor eget turpis cursus luctus. Mauris vestibulum rhoncus risus, id vulputate orci rutrum nec. Sed eleifend mi sed felis congue pharetra. Integer volutpat dapibus elit, vel feugiat mauris euismod a. Duis eget arcu sed ligula aliquam vestibulum.</p>
+  <div class="home">
+    <h1>
+        Bonjour
+        <span v-if="showProfile">Anne Moncion </span>
+        !
+    </h1>
+    <p v-if="!showProfile">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at elementum turpis, id pharetra arcu. Aenean egestas faucibus felis, suscipit tempus felis condimentum consequat. Sed vel dui turpis. Nam vitae dui vitae mi lacinia congue. Donec et magna nec odio iaculis euismod. Sed sit amet pharetra ante. Suspendisse tincidunt pulvinar ligula, pellentesque porta odio sodales sed. Donec iaculis nibh nec lectus pellentesque, vel euismod metus lobortis. Sed tincidunt tortor eget turpis cursus luctus. Mauris vestibulum rhoncus risus, id vulputate orci rutrum nec. Sed eleifend mi sed felis congue pharetra. Integer volutpat dapibus elit, vel feugiat mauris euismod a. Duis eget arcu sed ligula aliquam vestibulum.</p>
+    <div v-if="showProfile" class="profile">
+        <p><strong>Nom:</strong> Anne Moncion</p>
+        <p><strong>Âge:</strong> 27 ans</p>
+        <p><strong>Profession:</strong> Développeuse Front-end</p>
+        <p><strong>Passe-temps:</strong> Couture</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Footer',
+  name: 'Accueil',
+  data() {
+      return {
+          showProfile: false,
+      }
+  },
+  created() {
+    let loggedIn = false;
+  
+    if (loggedIn) {
+        this.showProfile = true;
+        console.log(this.showProfile);
+    }
+  }
 }
 </script>
 
 <style scoped>
 
-.accueil {
+.home {
     padding: 2rem;
     min-height: 60vh;
     max-width: 1200px;
     margin: 0 auto;
+}
+
+.profile {
+    background-color: palegreen;
+    padding: 4rem 1rem;
 }
 
 p {
---
+++ app-examen/src/views/Profil.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="profil">
+    <h1>Mon profil</h1>
+    <div class="infos">
+        <p><strong>Nom:</strong> Anne Moncion</p>
+        <p><strong>Âge:</strong> 27 ans</p>
+        <p><strong>Profession:</strong> Développeuse front-end</p>
+        <p><strong>Passions:</strong> La danse swing, la couture, la guitare</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+
+.profil {
+    padding: 2rem;
+    min-height: 60vh;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+p {
+    margin: 0 auto;
+    max-width: 600px;
+}
+
+</style>
